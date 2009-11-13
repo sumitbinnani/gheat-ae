5b7ab34212e17bf0442ac2b68ac7ec7cd0633f00
kranasian <kranasian@5baf47ec-bc51-11de-869a-473eb4776397>
2009-11-13 16:19:55 +0000

    Dan: Partially working commit

--- gHeatAE/src/gheatae/color_scheme.py
+++ gHeatAE/src/gheatae/color_scheme.py
@@ -1,5 +1,7 @@
 import pngcanvas
 
+TRANSPARENCY = 100
+
 def createScheme(steps=255, r_start=255, g_start=255, b_start=255, 
     r_step=-3.0, g_step=-1.0, b_step=-1.0):
   img = pngcanvas.PNGCanvas(30, steps)
@@ -8,7 +10,7 @@ def createScheme(steps=255, r_start=255, g_start=255, b_start=255,
   b_cur = b_start
   for y in range(0, steps):
     for x in range(0, 30):
-      img.canvas[y][x] = [ r_cur, g_cur, b_cur, 255]
+      img.canvas[y][x] = [ r_cur, g_cur, b_cur, TRANSPARENCY]
     r_cur += r_step
     g_cur += g_step
     b_cur += b_step
@@ -18,6 +20,6 @@ def createScheme(steps=255, r_start=255, g_start=255, b_start=255,
     if g_cur < 0: g_step *= -1; g_cur = 0 + g_step;
     if b_cur > 255: b_step *= -1; b_cur = 255 + b_step;
     if b_cur < 0: b_step *= -1; b_cur = 0 + b_step;
-  return img.dump()
+  return img
 
 cyan_red = createScheme()
